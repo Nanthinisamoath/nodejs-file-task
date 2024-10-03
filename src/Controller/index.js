3683cf220b6fb7622dfe44a84ea8c2e2f293cf03
Nanthinisamoath <nanthinisampath3009@gmail.com>
2024-10-03 16:58:21 +0530

    bug fixed

---
+++ src/Controller/index.js
@@ -0,0 +1,12 @@
+
+import express from 'express';
+
+import FileService from '../Service/FileService.js';
+
+const fileHandler = express();
+
+fileHandler.get('/read',FileService.readFile);
+
+fileHandler.post('/write',FileService.writeFile);
+
+export default fileHandler
---
+++ src/Service/FileService.js
@@ -0,0 +1,63 @@
+
+import * as fs from 'node:fs';
+
+const writeFile = (req,res) => {
+    try{
+        
+        const date = new Date();
+        let dateval = `${date.getMonth()+1}-${date.getDate()}-${date.getFullYear()}`;
+        let timeval =`${date.getHours()}H_${date.getMinutes()}M_${date.getSeconds()}S`;
+        let fileName = `${dateval}-${timeval}`;
+        let filePath = `./output/${fileName}.txt`;
+        //writing file
+        fs.writeFile(filePath,`Date:${dateval} Time:${timeval}`,'utf-8',(err)=>{
+            if(err) throw err            
+        })
+        res.status(201).send({
+            message: "File write successful"
+        })
+    }
+
+    catch(err) {
+       
+        console.error(err);
+    }
+    
+}
+
+const readFile = (req,res) => {
+    try{
+        let fileData = [];
+        //reading file
+         fs.readdir('./output',(err,data)=>{
+            if(err){
+                
+                console.error(err);
+            }
+            else if(data){
+                fileData=data.map((e)=>{
+                   return e;
+                    
+                })
+                res.status(200).send({
+                    message:"File read successful",
+                    data: fileData
+                })                
+            }
+                      
+        })
+        
+        
+            
+        
+        
+       
+    }
+    catch(err) {
+      
+        console.error(err)
+    }
+
+}
+
+export default {writeFile, readFile}
